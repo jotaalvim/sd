5e835508fcac16e9e0ce115be755bde86769cbb2
jotaalvim <joaoafonsoalvim@gmail.com>
2022-12-28 19:40:08 +0000

    base do trabalho?

---
+++ Aplicacao.java
@@ -0,0 +1,8 @@
+
+public class Aplicacao {
+    
+    public static void main(String[] args) {
+        Mapa a = new Mapa(5);
+    }
+}
+
--- Mapa.java
+++ Mapa.java
@@ -1,21 +1,61 @@
-
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Mapa {
-    
-    private Integer n;
 
-    List<List<Trotinetes>> trotis;
-    List<Trotinetes> trotinetes;
+    private Integer n;
+    private List< List<Integer> > mapa;
 
     public Mapa(Integer n) {
         this.n = n;
-        this.trotis = new ArrayList<>();
-        this.trotinetes = new ArrayList<>();
+        this.mapa = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                this.mapa.get(i).add(0);
+            }
+        }
+    }
+
+    public Mapa(Mapa a) {
+        this.n = a.n;
+        this.mapa = a.getMapa();
+    }
+
+    public Integer getN() {
+        return this.n;
+    }
+
+    public List<List<Integer>> getMapa() {
+        List<List<Integer>> copy = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            copy.add( new ArrayList<>() );
+            for (int j = 0; j < n; j++) {
+                int k = this.mapa.get(i).get(j);
+                copy.get(i).add(j);
+            }
+        }
+        return copy;
+    }
 
 
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(this.mapa.get(i).get(j));
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+    public void acrescenta (Integer x, Integer y) {
+        // inserir numa siteo
+        //this.mapa.get(y).get(x) =   
     }
 
 }
